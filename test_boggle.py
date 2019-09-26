460dbbbcbd9f9edcece44713e57d3ea504997444
Ubuntu <ubuntu@ip-172-31-94-6.ec2.internal>
2019-09-26 11:05:32 +0000

    Add grid for the test and import boggle

--- test_boggle.py
+++ test_boggle.py
@@ -1,5 +1,15 @@
 import unittest
+import boggle
 
-class test_boggle(unittest.TestCase):
-    def test_is_this_thing_on(self):
-        self.assertEqual(1, 1)
+class TestBoggle(unittest.TestCase):
+   """
+   The test suite for boggle solver
+   """
+   
+   def test_can_create_an_empty_grid(self):
+       """
+       Test to see if I can create an empty grid
+       """
+       grid = boggle.make_grid(0, 0) #we pass two arguments height and width.The simplest grid is one with no rows or columns so 0 0 is used.
+       self.assertEqual(len(grid), 0)
+   
